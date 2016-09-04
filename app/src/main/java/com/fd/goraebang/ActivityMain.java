be84545579af021cb4e401694ca0cf8fb3dea1e9
Park <bbayoung7849@gmail.com>
2016-09-04 21:01:56 +0900

    initialize project

---
+++ app/src/main/java/com/fd/goraebang/ActivityMain.java
@@ -0,0 +1,13 @@
+package com.fd.goraebang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
